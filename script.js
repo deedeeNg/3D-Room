4df9c2a31dae02dfffd21a77e05e85682baf59cb
The Nguyen <thenguyen@d207-023-200-049.wireless.sfu.ca>
2022-06-23 15:00:44 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('.btn').addEventListener('click',()=>{
+    document.querySelector('.table').classList.toggle("change");
+    document.querySelector('.letter').classList.toggle("change");
+});
+
+
+
+
+
